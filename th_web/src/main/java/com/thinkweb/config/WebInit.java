dbb52a75b4ed9661a00d674e346857dce8a900fa
wanglei <624432528@qq.com>
2015-09-25 08:55:37 +0800

    web config

---
+++ th_web/src/main/java/com/thinkweb/config/WebInit.java
@@ -0,0 +1,23 @@
+package com.thinkweb.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Created by juepei on 2015/9/25.
+ */
+public class WebInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{WebConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
