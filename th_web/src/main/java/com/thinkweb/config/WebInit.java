5c459320ee65e3e919192584c4d9af849127bffe
wanglei <624432528@qq.com>
2015-09-25 09:15:51 +0800

    change encoding

--- th_web/src/main/java/com/thinkweb/config/WebInit.java
+++ th_web/src/main/java/com/thinkweb/config/WebInit.java
@@ -18,6 +18,6 @@ public class WebInit extends AbstractAnnotationConfigDispatcherServletInitialize
 
     @Override
     protected String[] getServletMappings() {
-        return new String[]{"/*"};
+        return new String[]{"/"};
     }
 }
