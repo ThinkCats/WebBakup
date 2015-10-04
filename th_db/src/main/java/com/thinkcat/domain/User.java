17a2d0e30a2957ccf44a75a027a30602eafad1e6
wanglei <624432528@qq.com>
2015-10-05 01:31:59 +0800

    user

--- th_db/src/main/java/com/thinkcat/domain/User.java
+++ th_db/src/main/java/com/thinkcat/domain/User.java
@@ -1,1 +1,46 @@
-test
+package com.thinkcat.domain;
+
+/**
+ * Created by Think Cat on 15/10/4.
+ */
+
+public class User {
+    private Long uId;
+    private String userName;
+    private String password;
+    private Integer userType;
+
+
+    public Long getuId() {
+        return uId;
+    }
+
+    public void setuId(Long uId) {
+        this.uId = uId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getUserType() {
+        return userType;
+    }
+
+    public void setUserType(Integer userType) {
+        this.userType = userType;
+    }
+}
+	
