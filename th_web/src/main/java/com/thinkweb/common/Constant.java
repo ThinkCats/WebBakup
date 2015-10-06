7816becb9f510ed72496cde1564ca97a10524a67
wanglei <624432528@qq.com>
2015-10-06 11:19:54 +0800

    login update

---
+++ th_web/src/main/java/com/thinkweb/common/Constant.java
@@ -0,0 +1,8 @@
+package com.thinkweb.common;
+
+/**
+ * Created by Think Cat on 15/10/6.
+ */
+public class Constant {
+    public static final String BALNKUSER = "anonymousUser";
+}
--- th_web/src/main/java/com/thinkweb/controller/LoginController.java
+++ th_web/src/main/java/com/thinkweb/controller/LoginController.java
@@ -1,5 +1,9 @@
 package com.thinkweb.controller;
 
+import com.thinkweb.common.Constant;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,8 +17,13 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class LoginController {
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(){
-        return "login";
+    public String login(HttpSession session){
+        //TODO java spring security user remeber me
+        Object o = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (Constant.BALNKUSER.equals(o.toString())){
+            return "login";
+        }
+        return "index";
     }
 
     @RequestMapping(value = "/logout",method = RequestMethod.POST)
